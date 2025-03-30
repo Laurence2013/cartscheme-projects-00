93c9f3a9eb066a8350c12c42c35a220736d7aeab
Laurence2013 <lozza244@protonmail.com>
2025-03-30 10:24:59 +0100

    00-commit

---
+++ README.md
@@ -0,0 +1,5 @@
+System specifications:
+
+pwd -> .../projects/cartscheme-projects
+
+Node.js -> v22.13.1 Operating system: -Distributor ID: Ubuntu -Description: Ubuntu 20.04.6 LTS -Release: 20.04 -Codename: focal NPM -> 10.9.0 NVM -> 0.39.3
---
+++ proj00/npm_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Up to date on 25/02/25
+# Up to date on 24/03/25
+	# Change @tsconfig/node18@latest -> @tsconfig/node22@latest
+# Check at README.md for specifications
+
+npm install --save-dev typescript@latest typescript-eslint@latest ts-node@latest ts-jest@latest
+npm install --save-dev rxjs@latest rimraf@latest nodemon@latest newman@latest morgan@latest mongodb-memory-server@latest lint-staged@latest
+npm install --save-dev mongoose@latest joi@latest jest@latest immutable@latest husky@latest helmet@latest globals@latest express@latest
+npm install --save-dev eslint-plugin-mocha@latest eslint-plugin-jest@latest eslint@latest dotenv@latest debug@latest cookie-parser@latest
+npm install --save-dev concurrently@latest chai-as-promised@latest chai@latest
+npm install --save-dev @types/node@latest @types/morgan@latest @types/mongoose@latest @types/jest@latest @types/http-errors@latest
+npm install --save-dev @types/express@latest @types/debug@latest @types/cookie-parser@latest @types/chai-as-promised@latest @eslint/js@latest
+npm install --save-dev @tsconfig/node22@latest
