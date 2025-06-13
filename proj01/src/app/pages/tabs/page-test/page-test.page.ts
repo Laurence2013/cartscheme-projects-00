b45b750a490509ba9465f7cd8f6f300389b77bc6
Laurence2013 <lozza244@protonmail.com>
2025-06-13 13:16:50 +0100

    14-commit

--- proj01/src/app/pages/tabs/page-test/page-test.page.ts
+++ proj01/src/app/pages/tabs/page-test/page-test.page.ts
@@ -1,21 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonButton, IonActionSheet, IonRow, IonCol } from '@ionic/angular/standalone';
+
+import { ActionSheetController } from '@ionic/angular/standalone';
+import { 
+  IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, 
+  IonButton, IonActionSheet, IonRow, IonCol 
+} from '@ionic/angular/standalone';
+import { searchOutline, trash } from 'ionicons/icons';
 
 @Component({
   selector: 'app-page-test',
   templateUrl: './page-test.page.html',
   styleUrls: ['./page-test.page.scss'],
   standalone: true,
-  imports: [IonCol, IonRow, IonActionSheet, IonButton, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonCol, IonRow, IonActionSheet, IonButton, IonGrid, IonContent, 
+    IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule
+  ]
 })
 export class PageTestPage implements OnInit {
 
+  public isActionSheetOpen = false;
   public actionSheetButtons = [
-
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {action: 'delete'}
+    },
+    {
+      text: 'Share',
+      data: {action: 'share'}
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {action: 'cancel'}
+    },
+    {
+      text: 'Go back to Search',
+      data: {action: 'search'},
+      handler: () => {
+        this.router.navigate(['/tabs/search']);
+      }
+    }
   ]
-
-  public constructor(){}
+  public constructor(
+    private router: Router, 
+    private actionSheetController: ActionSheetController){}
   public ngOnInit(){}
+  public async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Actions',
+      subHeader: 'Choose an action',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: trash,
+          handler: () => console.log('Delete clicked')
+        },
+        {
+          text: 'Go back to Search',
+          data: {action: 'search'},
+          icon: searchOutline,
+          handler: () => this.router.navigate(['/tabs/search'])
+        }
+      ]
+    });
+    await actionSheet.present();
+  };
+  public setOpen(isOpen: boolean){
+    this.isActionSheetOpen = isOpen;
+  }
 }
