296e4ce5e4da4bebfd4e5b3b7365b968c74fa412
lorenzo187 <lozza244@protonmail.com>
2025-07-01 07:52:09 +0100

    42-commit

--- cheqprice00/src/app/components/top-value-added/top-value-added.component.ts
+++ cheqprice00/src/app/components/top-value-added/top-value-added.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { addIcons } from 'ionicons';
-import { IonButton, IonCard, IonItem, IonLabel, IonText, IonThumbnail, IonBadge } from '@ionic/angular/standalone';
+import { IonButton, IonCard, IonItem, IonLabel, IonText, IonThumbnail, IonBadge, IonList } from '@ionic/angular/standalone';
 
 import { ViewAllCardComponent } from '../../components/view-all-card/view-all-card.component';
 import { MainValueAdded00 } from '../../interfaces/value-added/main-value-added00.interface';
@@ -11,9 +11,9 @@ import { MainValueAdded00 } from '../../interfaces/value-added/main-value-added0
   selector: 'app-top-value-added',
   templateUrl: './top-value-added.component.html',
   styleUrls: ['./top-value-added.component.scss'],
-	imports: [IonButton, IonCard, IonItem, IonLabel, IonText, IonThumbnail, IonBadge, RouterLink, ViewAllCardComponent]
+	imports: [IonButton, IonCard, IonItem, IonLabel, IonText, IonThumbnail, IonBadge, IonList, RouterLink, ViewAllCardComponent]
 })
-export class TopValueAddedComponent  implements OnInit {
+export class TopValueAddedComponent implements OnInit {
   
 	public topValueAdded = input<MainValueAdded00>();
 	public isViewAll = input<boolean>(false);
@@ -21,4 +21,26 @@ export class TopValueAddedComponent  implements OnInit {
 
   public constructor(){}
   public ngOnInit(){}
+  public truncateText00(text: string[] | undefined, maxLength: number): string[] {
+    if (!text) return [];
+    if (text.length > maxLength) {
+      let truncated = text.map(text00 => text00.substring(0, maxLength));
+      const lastSpaceIndex = truncated.lastIndexOf(' ');
+      if (lastSpaceIndex !== -1) truncated = truncated.map(text => text.substring(0, lastSpaceIndex));
+      return [truncated + '...']; // Using '..' as you requested
+    }
+    return text;
+  }
+  public truncateText01(text: string | undefined, maxLength: number): string {
+    if (!text) return '';
+    if (text.length > maxLength) {
+      let truncated = text.substring(0, maxLength);
+      const lastSpaceIndex = truncated.lastIndexOf(' ');
+      if (lastSpaceIndex !== -1) {
+        truncated = truncated.substring(0, lastSpaceIndex);
+      }
+      return truncated + '...'; // Using '..' as you requested
+    }
+    return text;
+  }
 }
