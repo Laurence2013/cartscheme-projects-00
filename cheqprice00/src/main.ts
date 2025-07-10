1655c4e631a931aced1a4c05433e4abe108878c5
lorenzo187 <lozza244@protonmail.com>
2025-07-10 23:24:05 +0100

    72-commit

--- cheqprice00/src/main.ts
+++ cheqprice00/src/main.ts
@@ -1,11 +1,19 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
+import { getAuth, provideAuth, connectAuthEmulator } from '@angular/fire/auth';
+
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+if(environment.production){
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
