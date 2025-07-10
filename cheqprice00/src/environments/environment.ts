23a981a236cd0888de75a5898cf3b4098d608624
lorenzo187 <lozza244@protonmail.com>
2025-07-10 22:26:11 +0100

    70-commit

--- cheqprice00/src/environments/environment.ts
+++ cheqprice00/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  useEmulator: true,
+  firebase: {
+    apiKey: "AIzaSyAWncrwvhtOQZYuwyjvtC1qn_D-zOgKa4s",
+    authDomain: "cheqprice00.firebaseapp.com",
+    projectId: "cheqprice00",
+    storageBucket: "cheqprice00.firebasestorage.app",
+    messagingSenderId: "683606329020",
+    appId: "1:683606329020:web:be5d1202b619fb5ae0f2b9"
+  }
 };
 
 /*
--- cheqprice00/src/main.ts
+++ cheqprice00/src/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
+//import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
