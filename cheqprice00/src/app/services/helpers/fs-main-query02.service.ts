fc44d57d91d37a8a463af61692fbed6f0b5b61d5
lorenzo187 <lozza244@protonmail.com>
2025-07-20 20:42:01 +0100

    98-commit

--- cheqprice00/src/app/services/helpers/fs-main-query02.service.ts
+++ cheqprice00/src/app/services/helpers/fs-main-query02.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NgZone } from '@angular/core';
 
 import { Observable, EMPTY, from, of } from 'rxjs';
-import { tap, map, take } from 'rxjs/operators';
+import { tap, map, take, switchMap } from 'rxjs/operators';
 
 import { 
   Firestore, CollectionReference, collection, collectionData,
@@ -12,32 +12,40 @@ import {
 export class FsMainQuery02Service {
 
   public constructor(private ngZone: NgZone, private firestore: Firestore){}
-  public getValueAdded00(collectionName: string, documentType: string){
-    if(documentType === 'cashbacks') this.getCashbacks00(collectionName, documentType, this.getColParentID00.bind(this));
+  public getValueAdded00(collectionName: string, documentType: string): Observable<DocumentData[]> {
+    if(documentType === 'cashbacks') {
+      return this.getCashbacks00(collectionName, documentType, this.getColParentID00.bind(this));
+    }
     if(collectionName === 'loyalty'){}
+    return EMPTY;
   }
   private getCashbacks00(
     collectionName: string, 
     documentType: string,
     getColParentID: (collectionName: string) => Observable<string[]>): Observable<DocumentData[]> {
     const test00$: Observable<string[]> = getColParentID(collectionName);
-    test00$.subscribe();
-    return EMPTY;
+    const test01$: Observable<DocumentData[]> = test00$.pipe(
+      switchMap((data00: any) => this.getMainCollection00(collectionName, data00[0], documentType)));
+    return test01$;
   }
   private getColParentID00(collectionName: string): Observable<string[]> {
       const myCollectObjs00: CollectionReference<DocumentData> = collection(this.firestore, collectionName);
       return from(getDocs(myCollectObjs00)).pipe(
-        map((data00: QuerySnapshot<DocumentData>) => data00.docs.map(doc => doc.id)),
-        tap(data01 => console.log('Line 31: ', data01))
-      );
+        map((data00: QuerySnapshot<DocumentData>) => data00.docs.map(doc => doc.id)));
   }
-  private getMainCollection00(collectionName: string, parentId: string, documentType: string): DocumentData {
+  private getMainCollection00(collectionName: string, parentId: string, documentType: string): Observable<DocumentData[]> {
     const myCollectObjs00$: CollectionReference<DocumentData> = collection(
       this.firestore,
       collectionName,
       parentId,
       documentType
     )
-    return EMPTY;
+    const test00$: Observable<DocumentData[]> = from(getDocs(myCollectObjs00$)).pipe(
+      map((data00: QuerySnapshot<DocumentData>) => {
+        const discounts: DocumentData[] = [];
+        data00.forEach(doc => discounts.push({id: doc.id, ...doc.data()}));
+        return discounts;
+      }));
+    return test00$;
   }
 }
